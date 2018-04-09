8549f1b59b54620f23f68e4f32cefefeabaa496b
Walter <relw1wa-github@yahoo.de>
2018-04-09 20:09:01 +0200

    Put function to create QuoteContainer div inside setupQuoteContainer function
    Add Foundation's cell grid class to container element

--- js/rqm.js
+++ js/rqm.js
@@ -5,18 +5,16 @@ function RQM() {
 
 	this.initializeRQM = () => {
 		RQMRoot = document.querySelector('#rqm-root');
-		this.setupQuoteContainer(
-			function containerDiv() {
-				let container = document.createElement("div");
-				container.className = "continaer callout";
-				container.innerHTML = "<p>hello world</p>";
-				RQMRoot.appendChild(conatiner);
-			}
-		);
+		this.setupQuoteContainer();
 		this.setupNextQuoteBtn();
 	};
 	
-	this.setupQuoteContainer = () => {};
+	this.setupQuoteContainer = () => {
+		let container = document.createElement("div");
+		container.className = "cell callout";
+		container.innerHTML = "<p>hello world</p>";
+		RQMRoot.appendChild(container);
+	};
 	
 	this.setupNextQuoteBtn = () => {
 		
