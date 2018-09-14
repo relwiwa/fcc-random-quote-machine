5f3ab05edc4eae0b941925ebe23b70ff8300078f
Walter <relw1wa-github@yahoo.de>
2018-09-14 22:19:01 +0100

    Write tests for RQM's initial render concerning child components and make them pass

--- components/RQM.js
+++ components/RQM.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import IconButton from '../../../reusable-components/icon-button';
+
 const RQM = (props) => <div className="rqm grid-container">
-  <div className="grid-x grid-padding-x text-center">
-    <h1>Random Quote Machine</h1>
+  <div className="grid-x grid-padding align-center">
+    <h1 className="cell text-center">Random Quote Machine</h1>
+    <div className="quote-container cell medium-8 large-6 callout">
+      <div className="grid-x grid-padding">
+        <div className="quote cell">Quote</div>
+        <div className="author cell text-right">- Author</div>
+      </div>
+    </div>
+    <div className="cell button-group align-center">
+      <IconButton
+        text="Tweet Quote"
+        faIcon={['fab', 'twitter']}
+        className="tweet-quote"
+      />
+      <IconButton
+        text="New Quote"
+        faIcon="redo"
+        className="new-quote"
+      />
+    </div>
   </div>
+  <div className="cell api-reference text-center">All quotes are retrieved from Quotes on Design API</div>
 </div>;
 
 export default RQM;
--- components/RQM.test.js
+++ components/RQM.test.js
@@ -10,4 +10,26 @@ describe('<RQM>', () => {
     wrapper = shallow(<RQM />);
   });
 
+  describe('when rendering initially', () => {
+    describe('concerning its child elements', () => {
+      it('renders a .quote-container', () => {
+        expect(wrapper.find('.quote-container')).toHaveLength(1);
+      });
+      it('renders a .quote inside .quote-container', () => {
+        expect(wrapper.find('.quote-container .quote')).toHaveLength(1);
+      });
+      it('renders an .author inside .quote-container', () => {
+        expect(wrapper.find('.quote-container .author')).toHaveLength(1);
+      });
+      it('renders a .tweet-quote IconButton', () => {
+        expect(wrapper.find('IconButton.tweet-quote')).toHaveLength(1);
+      });
+      it('renders a .new-quote IconButton', () => {
+        expect(wrapper.find('IconButton.new-quote')).toHaveLength(1);
+      });
+      it('renders an .api-reference', () => {
+        expect(wrapper.find('.api-reference')).toHaveLength(1);
+      });
+    });
+  });
 });
