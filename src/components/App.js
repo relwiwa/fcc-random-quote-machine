168a20f85decb18a31608bcc67a61e14cb84042d
J0ssue <jdiaz66@icloud.com>
2018-09-15 17:22:03 +0100

    "added button"

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import QuoteContainer from './QuoteContainer';
+import Button from './Button';
 
 class App extends Component {
 	constructor(props) {
@@ -9,7 +10,15 @@ class App extends Component {
 
 		this.state = {
 			quote: 'hello World',
-			author: 'Josue'
+			author: 'Josue',
+			twitter: {
+				text: 'Twiter',
+				icon: 'icon'
+			},
+      newQuote: {
+        text: 'New Quote',
+        icon: 'icon'
+      }
 		};
 	}
   render() {
@@ -19,6 +28,8 @@ class App extends Component {
           <h1 className="cell">Random Quote Machine</h1>
         </div>
         <QuoteContainer author={this.state.author} quote={this.state.quote} />
+        <Button text={this.state.newQuote.text} icon={this.state.newQuote.icon} className="twitter" />
+        <Button text={this.state.twitter.text} icon={this.state.twitter.icon} className="new-quote" />
       </div>
     );
   }
